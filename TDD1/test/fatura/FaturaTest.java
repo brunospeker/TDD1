cc79cb73cce4b5bcf815991d5889f02b9423f820
Bruno Speker <bruno.speker@gmail.com>
2020-10-05 08:44:28 -0300

    Refatoração da FaturaTest

--- TDD1/test/fatura/FaturaTest.java
+++ TDD1/test/fatura/FaturaTest.java
@@ -7,10 +7,10 @@ public class FaturaTest {
 
 	@Test
 	public void testFatura() {
-		Fatura fat = new Fatura("04/06/2020","Bruno",300.00);
+		Fatura fat = new Fatura("04/06/2020","Bruno",400.00);
 		
 		Assertions.assertEquals("04/06/2020", fat.getDataFatura());
 		Assertions.assertEquals("Bruno", fat.getNomeClienteFatura());
-		Assertions.assertEquals(300, fat.getValorTotalFatura());
+		Assertions.assertEquals(400, fat.getValorTotalFatura());
 	}
 }
