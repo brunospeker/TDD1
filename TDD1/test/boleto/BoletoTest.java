a8b266f90521b2aa18cd752943d1304a7d02e44f
Bruno Speker <bruno.speker@gmail.com>
2020-10-05 08:53:27 -0300

    Correção com o minimo para a classe BoletoTest passar

--- TDD1/test/boleto/BoletoTest.java
+++ TDD1/test/boleto/BoletoTest.java
@@ -8,7 +8,7 @@ public class BoletoTest {
 	@Test
 	public void testBoleto() {
 	
-	Boleto boletotest = new Boleto("2103","21/03/2020", 500.00);
+	Boleto boletotest = new Boleto("2103","21/03/2020", 200.00);
 	
 	Assertions.assertEquals("2103", boletotest.getCodBoleto());
 	Assertions.assertEquals("21/03/2020", boletotest.getDataBoleto());
