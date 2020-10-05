edf3f699eb5f520e4704d2419765638c59fa582b
Bruno Speker <bruno.speker@gmail.com>
2020-10-05 08:37:57 -0300

    Adição da classe de teste Fatura com erro / iniciando o TDD

---
+++ TDD1/test/fatura/FaturaTest.java
@@ -0,0 +1,16 @@
+package fatura;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FaturaTest {
+
+	@Test
+	public void testFatura() {
+		Fatura fat = new Fatura("04/06/2020","Bruno",300.00);
+		
+		Assertions.assertEquals("04/06/2020", fat.getDataFatura());
+		Assertions.assertEquals("Bruno", fat.getNomeClienteFatura());
+		Assertions.assertEquals(200, fat.getValorTotalFatura());
+	}
+}
